90be84df8141bee8137dd23ab5db623e6e26174b
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-10-04 16:17:20 +0530

    server setup done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,10 @@
+const { supabase } = require("../db/dbConfig");
+
 module.exports.login = async (req, res) => {
   res.status(200).json({ result: "working successfully" });
+};
+
+module.exports.getAllUsers = async (req, res) => {
+  let { data: Users, error } = await supabase.from("Users").select("*");
+  res.status(200).json({ result: Users });
 };
---
+++ db/dbConfig.js
@@ -0,0 +1,7 @@
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+module.exports.supabase = createClient(
+  process.env.SUPABASE_REST_URI,
+  process.env.SUPABASE_SERVICE_ROLE_API_KEY
+);
