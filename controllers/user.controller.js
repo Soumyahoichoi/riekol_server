7a0a4aff9bb5799d9d007655b39798d2cc96ced4
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-10-06 15:18:09 +0530

    stripe integrated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,19 +1,40 @@
-const { supabase } = require('../db/dbConfig');
+const { supabase } = require("../db/dbConfig");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports.login = async (req, res) => {
-	const { email } = req.body;
+  const { email } = req.body;
 
-	console.log(req.body);
-	const { data, error } = await supabase.from('Users').select().eq('email', email);
-	res.status(200).json({ result: data, error });
+  console.log(req.body);
+  const { data, error } = await supabase
+    .from("Users")
+    .select()
+    .eq("email", email);
+  res.status(200).json({ result: data, error });
 };
 
 module.exports.getAllUsers = async (req, res) => {
-	let { data: Users, error } = await supabase.from('Users').select('*');
+  let { data: Users, error } = await supabase.from("Users").select("*");
 
-	if (Users) {
-		res.status(200).json({ result: Users });
-	} else if (error) {
-		res.status(500).json({ error });
-	}
+  if (Users) {
+    res.status(200).json({ result: Users });
+  } else if (error) {
+    res.status(500).json({ error });
+  }
+};
+
+module.exports.createSession = async (req, res) => {
+  const { cartValues } = req.body;
+  const line_items = cartValues.map((item) => ({
+    price: item.priceId,
+    quantity: item.count,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    success_url: `http://localhost:5173/deepdive`,
+    cancel_url: `http://localhost:5173/deepdive`,
+  });
+
+  res.redirect(303, session.url);
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-const { login, getAllUsers } = require('../controllers/user.controller');
+const express = require("express");
+const {
+  login,
+  getAllUsers,
+  createSession,
+} = require("../controllers/user.controller");
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/login', login);
-router.get('/getAllUsers', getAllUsers);
+router.post("/login", login);
+router.get("/getAllUsers", getAllUsers);
+router.post("/checkout", createSession);
 
 module.exports = router;
