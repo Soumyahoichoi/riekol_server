85b7be3cfa2ed4b45856de4a57d79d92434c3990
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-10-09 19:41:54 +0530

    serve changes fr stirpe

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const {
   login,
   getAllUsers,
   createSession,
+  generateClientSecret,
 } = require("../controllers/user.controller");
 var router = express.Router();
 
@@ -10,5 +11,6 @@ var router = express.Router();
 router.post("/login", login);
 router.get("/getAllUsers", getAllUsers);
 router.post("/checkout", createSession);
+router.post("/getSecet", generateClientSecret);
 
 module.exports = router;
