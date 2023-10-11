927a66f8cee699bc575b9578818ed2b2592af553
soumya <soumya.chakraborty@hoichoi.tv>
2023-10-11 14:00:26 +0530

    Database configuration and api creation

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { Pool } = require('pg');
-
-//
-const generateNewPool = () => {
-	return new Pool({
-		user: 'postgres',
-		host: 'db.kmzribvpzlhzpzlfvvml.supabase.co',
-		database: 'postgres',
-		password: 'zJwfctd8ihxPF3gO',
-		port: 5432
-	});
-};
-
-// TO send all the data from the database
-router.get('/api/data', async (req, res, next) => {
-	const pool = generateNewPool();
-	const response = await pool.query('SELECT * FROM my_eo_website_format');
-	const responseData = response.rows;
-	pool.end();
-	res.send(responseData);
-});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
