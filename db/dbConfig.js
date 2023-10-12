9e5e997305ed409771de6f7757d61b326c626c8e
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-10-12 13:35:11 +0530

    db changes for payment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,19 @@
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+const { Pool } = require("pg");
 
 module.exports.supabase = createClient(
-	process.env.SUPABASE_REST_URI,
-	process.env.SUPABASE_SERVICE_ROLE_API_KEY
+  process.env.SUPABASE_REST_URI,
+  process.env.SUPABASE_SERVICE_ROLE_API_KEY
 );
-
-const { Pool } = require('pg');
 
 //
 module.exports.generateNewPool = () => {
-	return new Pool({
-		user: 'postgres',
-		host: 'db.kmzribvpzlhzpzlfvvml.supabase.co',
-		database: 'postgres',
-		password: process.env.PASSWORD,
-		port: 5432
-	});
+  return new Pool({
+    user: "postgres",
+    host: "db.kmzribvpzlhzpzlfvvml.supabase.co",
+    database: "postgres",
+    password: process.env.PASSWORD,
+    port: 5432,
+  });
 };
