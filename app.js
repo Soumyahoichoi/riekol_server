5ea00b84100102d398657735178e2ae53fd9b336
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-12-07 13:20:05 +0530

    logger added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jsx');
 app.engine('jsx', engine);
 
 app.use(cors());
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
